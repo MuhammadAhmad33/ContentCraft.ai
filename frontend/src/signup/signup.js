5512ede9af81c33e338611cba6030e3f02759e8e
Muhammad Ahmad <100872369+MuhammadAhmad33@users.noreply.github.com>
2024-04-27 03:21:40 +0500

    signup component

---
+++ frontend/src/signup/signup.js
@@ -0,0 +1,76 @@
+import './signup.css'
+
+export default function SignUp() {
+    return (
+        <div className="sign-up-2">
+            <div className="header">
+                <div className="content-craft-ai">
+                    ContentCraft.ai
+                </div>
+                <div className="home">
+                    Home
+                </div>
+                <div className="container-1">
+                    <div className="back-1">
+                    </div>
+                    <div className="back">
+                        Back
+                    </div>
+                </div>
+                <div className="container-3">
+                    <span className="log-in">
+                        Log In
+                    </span>
+                </div>
+            </div>
+            <div className="sign-uo-account">
+                <p className="sign-up-to-create-your-account">
+                    <span className="sign-up-to-create-your-account-sub-1"></span><span></span>
+                </p>
+                <div className="email">
+                    Email
+                </div>
+                <div className="email-placeholder">
+                </div>
+                <div className="password">
+                    Password
+                </div>
+                <div className="password-placeholder">
+                </div>
+                <div className="confirm-password">
+                    Confirm Password
+                </div>
+                <div className="confirm-password-placeholder">
+                </div>
+                <div className="container-4">
+                    <div className="line-1">
+                    </div>
+                    <span className="or">
+                        OR
+                    </span>
+                    <div className="line-2">
+                    </div>
+                </div>
+                <div className="container-5">
+                    <div className="google">
+                    </div>
+                    <div className="sign-in-with-google">
+                        Sign In with Google
+                    </div>
+                </div>
+                <div className="container-2">
+                    <div className="linked-in">
+                    </div>
+                    <div className="sign-in-with-linked-in">
+                        Sign In with LinkedIn
+                    </div>
+                </div>
+                <div className="container">
+                    <span className="sign-up-1">
+                        Sign Up
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
