3074564b867a863d83d5c110eb28fd90d4e93e1a
Muhammad Ahmad <100872369+MuhammadAhmad33@users.noreply.github.com>
2024-04-27 02:47:43 +0500

    HomePage Added
    Homesscreen is added to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
+import React from 'react';
+import HomeScreen from './homePage/Homescreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/homescreen" element={<HomeScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/homePage/Homescreen.js
@@ -0,0 +1,146 @@
+import './Homescreen.css'
+
+export default function HomeScreen() {
+    return (
+        <div className="home-screen">
+            <div className="rectangle-26">
+            </div>
+            <div className="header">
+                <div className="content-craft-ai">
+                    ContentCraft.ai
+                </div>
+                <div className="log-in">
+                    Log In
+                </div>
+                <div className="container-11">
+                    <span className="sign-up">
+                        Sign Up
+                    </span>
+                </div>
+            </div>
+            <div className="container-7">
+                <div className="container-10">
+                    <div className="container-5">
+                        <div className="container-12">
+                            <span className="a">
+                                A
+                            </span>
+                        </div>
+                        <div className="account">
+                            Account
+                        </div>
+                    </div>
+                    <div className="container">
+                        <div className="new-chat">
+                            New chat
+                        </div>
+                        <div className="add-to-chat">
+                        </div>
+                    </div>
+                </div>
+                <div className="main-heading">
+                    <p className="welcome-to-content-craft-ai">
+                        <span className="welcome-to-content-craft-ai-sub-7"></span><span></span>
+                    </p>
+                    <span className="tailored-for-purpose-perfect-for-engagement">
+                        Tailored for Purpose, Perfect for Engagement
+                    </span>
+                </div>
+            </div>
+            <div className="container-13">
+                <p className="im-your-ai-assistant-here-to-make-creating-engaging-content-for-linked-in-instagram-and-twitter-effortless-just-tell-me-your-purpose-target-audience-and-platform-needs-and-ill-generate-personalized-top-notch-content-for-you-in-no-time">
+                    <span className="im-your-ai-assistant-here-to-make-creating-engaging-content-for-linked-in-instagram-and-twitter-effortless-just-tell-me-your-purpose-target-audience-and-platform-needs-and-ill-generate-personalized-top-notch-content-for-you-in-no-time-sub-15"></span><span className="im-your-ai-assistant-here-to-make-creating-engaging-content-for-linked-in-instagram-and-twitter-effortless-just-tell-me-your-purpose-target-audience-and-platform-needs-and-ill-generate-personalized-top-notch-content-for-you-in-no-time-sub-14"></span><span className="im-your-ai-assistant-here-to-make-creating-engaging-content-for-linked-in-instagram-and-twitter-effortless-just-tell-me-your-purpose-target-audience-and-platform-needs-and-ill-generate-personalized-top-notch-content-for-you-in-no-time-sub-15"></span><span className="im-your-ai-assistant-here-to-make-creating-engaging-content-for-linked-in-instagram-and-twitter-effortless-just-tell-me-your-purpose-target-audience-and-platform-needs-and-ill-generate-personalized-top-notch-content-for-you-in-no-time-sub-14"></span><span className="im-your-ai-assistant-here-to-make-creating-engaging-content-for-linked-in-instagram-and-twitter-effortless-just-tell-me-your-purpose-target-audience-and-platform-needs-and-ill-generate-personalized-top-notch-content-for-you-in-no-time-sub-15"></span><span className="im-your-ai-assistant-here-to-make-creating-engaging-content-for-linked-in-instagram-and-twitter-effortless-just-tell-me-your-purpose-target-audience-and-platform-needs-and-ill-generate-personalized-top-notch-content-for-you-in-no-time-sub-14"></span><span></span>
+                </p>
+                <div className="chatbot-1">
+                </div>
+            </div>
+            <div className="container-9">
+                <div className="history">
+                    History
+                </div>
+                <div className="all-options">
+                    <div className="container-2">
+                        <div className="target-audience">
+                            Target audience:
+                        </div>
+                        <div className="frame-2">
+                            <div className="age-group">
+                                Age group
+                            </div>
+                            <div className="expand-arrow-3">
+                            </div>
+                        </div>
+                        <div className="frame-3">
+                            <div className="gender">
+                                Gender
+                            </div>
+                            <div className="expand-arrow">
+                            </div>
+                        </div>
+                        <div className="frame-4">
+                            <div className="country">
+                                Country
+                            </div>
+                            <div className="expand-arrow-1">
+                            </div>
+                        </div>
+                        <div className="frame-5">
+                            <div className="field">
+                                Field
+                            </div>
+                            <div className="expand-arrow-2">
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container-4">
+                        <span className="hashtags">
+                            #Hashtags
+                        </span>
+                    </div>
+                    <div className="container-6">
+                        <div className="select-platform">
+                            Select platform:
+                        </div>
+                        <div className="container-8">
+                            <div className="unchecked-checkbox">
+                            </div>
+                            <div className="instagram">
+                                Instagram
+                            </div>
+                        </div>
+                        <div className="container-14">
+                            <div className="unchecked-checkbox-1">
+                            </div>
+                            <div className="twitter">
+                                Twitter
+                            </div>
+                        </div>
+                        <div className="container-3">
+                            <div className="unchecked-checkbox-2">
+                            </div>
+                            <div className="linked-in">
+                                LinkedIn
+                            </div>
+                        </div>
+                        <div className="container-15">
+                            <div className="unchecked-checkbox-3">
+                            </div>
+                            <div className="facebook">
+                                Facebook
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container-1">
+                        <div className="write-your-purpose">
+                            Write your purpose....
+                        </div>
+                        <div className="enter">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="line-3">
+            </div>
+        </div>
+    )
+}
